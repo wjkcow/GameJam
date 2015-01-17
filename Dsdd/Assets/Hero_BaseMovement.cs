169344c234f1894410d1863168e03c4eec1c6e32
Jingkui Wang <wjkcow@gmail.com>
2015-01-16 22:55:56 -0500

    jump walk and so on

---
+++ Dsdd/Assets/Hero_BaseMovement.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero_BaseMovement : MonoBehaviour {
+	bool isJumping = false;
+	bool jumpTurnLeft = false;
+	bool jumpTurnRight = false;
+	public Vector2 walkRightSpeed = new Vector2(2.0f,0.0f);
+	public Vector2 jumpRightSpeed = new Vector2(0.5f,0.0f);
+	public Vector2 jumpSpeed = new Vector2(0.0f, 10.0f);
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		jump ();
+		move ();
+	}
+
+	// jump when space is press down, left/right jump if Arrow pressed
+	// if in the air change direction if arrow pressed
+	void jump(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Vector2 speed = jumpSpeed;
+			isJumping = true;
+			if(Input.GetKey (KeyCode.LeftArrow)){
+				speed += -1 * jumpRightSpeed;
+			} else if (Input.GetKey (KeyCode.RightArrow)){
+				speed += jumpRightSpeed;
+			}
+			this.rigidbody2D.velocity = speed;
+		}
+		if(isJumping){
+			Vector2 speed = Vector2.zero;
+			if(Input.GetKey (KeyCode.LeftArrow)){
+				if(!jumpTurnLeft){
+					this.rigidbody2D.velocity = new Vector2(0.0f, this.rigidbody2D.velocity.y);
+					speed += -1 * jumpRightSpeed;
+					jumpTurnLeft = true;
+				}
+			} else if (Input.GetKey (KeyCode.RightArrow)){
+				if(!jumpTurnRight){
+					this.rigidbody2D.velocity = new Vector2(0.0f, this.rigidbody2D.velocity.y);
+					speed += jumpRightSpeed;
+					jumpTurnRight = true;
+				}
+			}
+			this.rigidbody2D.velocity += speed;
+		} 
+	}
+
+	// if not jumping move left or right
+	void move(){
+		if (!isJumping) {
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				this.transform.Translate(-1 * walkRightSpeed * Time.deltaTime);
+			}
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				this.transform.Translate(walkRightSpeed * Time.deltaTime);
+			}
+		}
+	}
+
+	void postJump(){
+		isJumping = false;
+		jumpTurnLeft = false;
+		jumpTurnRight = false;
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		print ("hit ground");
+		postJump ();
+	} 
+}
---
+++ Dsdd/Assets/Hero_shoot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero_shoot : MonoBehaviour {
+	public GameObject indicater;
+	public float indicaterDist = 0.2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		handleIndicater ();
+	}
+
+	void handleIndicater(){
+		indicater.transform.position = indicaterPos ();
+//		print (indicaterAngel ());
+//		var target = Quaternion.Euler(0,0,indicaterAngel ());
+//		//indicater.transform.rotation = Quaternion.Slerp (transform.rotation, target, Time.deltaTime);
+//		print (indicater.transform.rotation);
+	}
+	Vector2 mousePosition(){
+		Vector2 ret = new Vector2();
+		Vector3 mouPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		ret.x = mouPos.x;
+		ret.y = mouPos.y;
+		return ret;
+	}
+
+
+	Vector2 indicaterPos(){
+		Vector2 myPos = this.transform.position;
+		Vector2 mousePos = mousePosition ();
+		Vector2 v = mousePos - myPos;
+		return myPos + v * indicaterDist / Mathf.Sqrt (v.x*v.x + v.y*v.y);
+	}
+
+//	float shootAngel(){
+//		Vector2 myPos = this.transform.position;
+//		Vector2 mousePos = mousePosition ();
+//		Vector2 v = mousePos - myPos;
+//		return Mathf.Atan2(v.y , v.x) / Mathf.PI * 180;
+//	}
+}
