218722ab216b5006cbd5b29c9c144cbeafde222b
lslsheng <527873752@qq.com>
2015-01-16 22:55:42 -0500

    portal

---
+++ Dsdd/Assets/fire.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fire : MonoBehaviour {
+	public GameObject cubePrefabVar;
+	public GameObject global;
+	private float time ;
+	// Use this for initialization
+	void Start () {
+		time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > time + 1.0f){
+			Instantiate (cubePrefabVar);
+			time = Time.time;
+		}
+	}
+}
