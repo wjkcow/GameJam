c5d4255a24b266dce0973707771fa094d09e1f8b
ByzanTine <morbyzantine@gmail.com>
2015-01-18 02:22:28 -0500

    Start and end point for game

---
+++ Dsdd/Assets/Scripts/EntryNextLevel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntryNextLevel : MonoBehaviour {
+	private AnimationClip animClp;
+	// Use this for initialization
+	void Start () {
+		animClp = Resources.Load ("Rotate-shrink") as AnimationClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		GameObject tmp = other.gameObject;
+		if (!animClp)
+			Debug.LogError ("animation not loaded");
+		else {
+			// externally link animation
+			tmp.AddComponent<Animation>();
+			tmp.animation.AddClip (animClp, "Rotate-Shrink");
+			tmp.animation.Play ("Rotate-Shrink");
+			// enter next level now
+			ChangeLevel (tmp);
+		}
+	}
+
+	void ChangeLevel(GameObject player) {
+		// do whatever need to do
+		Debug.Log("Enter next level");
+		// clear up !
+	}
+}
---
+++ Dsdd/Assets/Scripts/StartGameByWhiteHole.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameByWhiteHole : MonoBehaviour {
+	private AnimationClip animClp;
+	private GameObject player;
+	void Start() {
+		animClp = Resources.Load("Rotate-shrink-reverse") as AnimationClip;
+		// Instaniate the Gameplayer or get it 
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+		if (!player.GetComponent<Animation>())
+			player.AddComponent<Animation> ();
+		// sync the position 
+		player.transform.position = transform.position;
+
+		player.animation.AddClip (animClp, "Rotate-shrink-reverse");
+		// play
+		gameObject.animation.Play ("Rotate-shrink-reverse");
+		player.animation.Play ("Rotate-shrink-reverse");
+
+		Destroy (gameObject, 1.0f);
+
+	}
+}
