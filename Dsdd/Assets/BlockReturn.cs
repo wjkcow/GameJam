874fe3c571793357e1d95a0ccbb69db47394c893
Jingkui Wang <wjkcow@gmail.com>
2015-01-17 22:46:13 -0500

    level

---
+++ Dsdd/Assets/BlockReturn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockReturn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > 0) {
+			Vector3 pos = transform.position;
+			pos.x = -0.4f;
+			transform.position = pos;
+		}
+	}
+}
