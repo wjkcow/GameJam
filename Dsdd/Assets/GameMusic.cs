1c7fcfa3adf4125906e40e2903178b3a200e40d0
robinbach <bestname@umich.edu>
2015-01-18 01:45:12 -0500

    lets play

--- Dsdd/Assets/GameMusic.cs
+++ Dsdd/Assets/GameMusic.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GameMusic : MonoBehaviour {
 
-	public AudioClip NewMusic;
+//	public AudioClip NewMusic;
 	private static GameMusic instance = null;
 	public static GameMusic Instance 
 	{
@@ -19,9 +19,9 @@ public class GameMusic : MonoBehaviour {
 		}
 		DontDestroyOnLoad(this.gameObject);
 
-		GameObject go = GameObject.Find("Game Music"); //Finds the game object called Game Music, if it goes by a different name, change this. 
-		go.audio.clip = NewMusic; 
-		//Replaces the old audio with the new one set in the inspector. 
-		go.audio.Play(); //Plays the audio.
+//		GameObject go = GameObject.Find("Game Music"); //Finds the game object called Game Music, if it goes by a different name, change this. 
+//		go.audio.clip = NewMusic; 
+//		//Replaces the old audio with the new one set in the inspector. 
+//		go.audio.Play(); //Plays the audio.
 	}
 }
