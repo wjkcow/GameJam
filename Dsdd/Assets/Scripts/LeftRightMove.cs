c84f6924c7faf6b7dddece347a350081512335d5
ByzanTine <morbyzantine@gmail.com>
2015-01-18 01:24:08 -0500

    Camera follow update. Add floating box script

---
+++ Dsdd/Assets/Scripts/LeftRightMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftRightMove : MonoBehaviour {
+
+	private float changeInterval;
+
+	void Start () {
+		changeInterval = Time.time;
+		rigidbody2D.velocity = new Vector2 (1, 0);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		if ( Time.time - changeInterval > 3.0f) {
+			changeInterval = Time.time;
+
+			rigidbody2D.velocity = -rigidbody2D.velocity;  
+
+		}
+
+	}
+}
