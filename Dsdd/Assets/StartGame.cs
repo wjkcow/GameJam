6aa01187b1afbb9882d638f12d90ba4fbb95544e
ByzanTine <morbyzantine@gmail.com>
2015-01-18 06:10:52 -0500

    Start and End Screen finished with scene transition.

---
+++ Dsdd/Assets/StartGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	public void StartGame_level() {
+		Application.LoadLevel("wjn_scene");
+	}
+	public void returnToTitle() {
+		Application.LoadLevel ("_Title_Screen");
+	}
+}
