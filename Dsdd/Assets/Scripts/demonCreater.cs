44005ce8f36d9700b86747caefd9d7db7361c41d
robinbach <bestname@umich.edu>
2015-01-17 21:31:50 -0500

    wjn's scene

---
+++ Dsdd/Assets/Scripts/demonCreater.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class demonCreater : MonoBehaviour {
+
+	public GameObject  demonPrefab;
+	private GameObject gameObj;
+	private int demonNum = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Time.frameCount % 25 == 0) {
+			GameObject gameObj = Instantiate (demonPrefab) as GameObject;
+			Vector3 pos = this.transform.position;
+			gameObj.transform.position = pos;
+			demonNum++;
+		}
+		if(demonNum >= 3)
+			Destroy(this.gameObject);
+	}
+}
---
+++ Dsdd/Assets/Scripts/demonCreator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class demonCreator : MonoBehaviour {
+	
+	public GameObject  demonPrefab;
+	private GameObject gameObj;
+	
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			for(int i=0; i<3; ++i) {
+				GameObject gameObj = Instantiate (demonPrefab) as GameObject;
+				Vector3 pos = this.transform.position;
+				pos.y += 3f;
+				pos.x += i - 1.5f;
+				gameObj.transform.position = pos;
+			}
+		}
+	}
+}
