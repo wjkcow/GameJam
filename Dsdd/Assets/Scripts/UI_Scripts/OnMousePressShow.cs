3698dd83a931b6f2ea310b1e1a8696d9576db94f
ByzanTine <morbyzantine@gmail.com>
2015-02-10 04:16:57 -0500

    Add Mirror Help Text. [Fix] a mirror placement bug in level_wjk

--- Dsdd/Assets/Scripts/UI_Scripts/OnMousePressShow.cs
+++ Dsdd/Assets/Scripts/UI_Scripts/OnMousePressShow.cs
@@ -4,16 +4,28 @@ using System.Collections;
 public class OnMousePressShow : MonoBehaviour {
 
 	private bool shown = false;
-	
+	public int sequence_id = 1;
 	void Update() {
 
 		if (Input.GetMouseButtonDown(1)) {
-			if (!shown && HelpTextControl.HelpTextindex == 1){
-				Debug.Log("HELP TEXT: enter trigger, show text");
-				GetComponentInParent<ShowHelpText>().showText();
-				shown = true;
-				HelpTextControl.incrementIndex(2.0f);
+			if (!shown){
+				StartCoroutine(showText());
 			}
 		}
+	}
+	IEnumerator showText() {
+		while (true) {
+			if (HelpTextControl.HelpTextindex != sequence_id) {
+				yield return new WaitForSeconds(0.3f);
+				continue;
+			}
+			Debug.Log("HELP TEXT: enter trigger, show text");
+			GetComponentInParent<ShowHelpText>().showText();
+			shown = true;
+			HelpTextControl.incrementIndex(2.0f);
+			shown = false;
+			break;
+		}
+
 	}
 }
